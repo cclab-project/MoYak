9d5d8aecf32ffc04f5abd1a67f44666a2ef2bd5c
SiHoon61 <dlatlgns000@gmail.com>
2024-06-26 17:41:05 +0900

    Add: create Main page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 //pages
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
+import Home from "./pages/Main/Main";
 
 function App() {
   return (
@@ -10,10 +11,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />}></Route>
+          <Route path="/Home" element={<Home />}></Route>
         </Routes>
       </Router>
     </>
-  );  
+  );
 }
 
 export default App;
