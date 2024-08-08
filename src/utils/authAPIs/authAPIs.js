cc2eb33e8f7cd50c7a363bf48fd102ba02dffbde
SiHoon61 <dlatlgns000@gmail.com>
2024-08-08 15:24:09 +0900

    Fix: kakaoLoginReq end-point

--- src/utils/authAPIs/authAPIs.js
+++ src/utils/authAPIs/authAPIs.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const kakaoLoginReq = async (code) => {
     axios.defaults.withCredentials = true;
     const response = await axios.post(
-        `${process.env.REACT_APP_SERVER_URL}/login/oauth2/code/kakao?code=${code}`
+        `${process.env.REACT_APP_SERVER_URL}/callback?code=${code}`
     );
 
     return response;
