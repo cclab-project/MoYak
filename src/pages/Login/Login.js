68cc183836c2d15d8ceff8ec1ab4cbb6cbfd3bfc
SiHoon61 <dlatlgns000@gmail.com>
2024-08-08 15:18:13 +0900

    Add: kakao, google Logo

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,9 +3,15 @@ import React from 'react';
 //link
 import { KAKAO_LINK } from '../../config/config';
 
+//img
+import kakao from '../../assets/Login/kakao.png'
+import google from '../../assets/Login/google.png'
+
 import {
     LogoBox,
     LoginContainer,
+    JoinContainer,
+    SNSIcon,
 } from './style';
 const Login = () => {
     const kakaoLoginHandler = () => {
@@ -17,9 +23,14 @@ const Login = () => {
             <LogoBox>
 
             </LogoBox>
-            <button onClick={kakaoLoginHandler}>
-                카카오 로그인
-            </button>
+            <JoinContainer>
+                <SNSIcon
+                    src={kakao}
+                    alt='kakao'
+                    onClick={kakaoLoginHandler}
+                />
+                <SNSIcon src={google} alt='google' />
+            </JoinContainer>
 
         </LoginContainer>
 
--- src/pages/Login/style.js
+++ src/pages/Login/style.js
@@ -13,3 +13,19 @@ export const LoginContainer = styled.div`
     display: flex;
     flex-direction: column;
 `
+
+export const JoinContainer = styled.div`
+    margin-top: 200px;
+    width: 100%;
+    height: 100px;
+    display: flex;
+    justify-content: space-evenly;
+`
+
+export const SNSIcon = styled.img`
+    width: 60px;
+    height: 60px;
+    border-radius: 15px;
+    box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.25);
+    cursor: pointer;
+`
