9082246aaa08591ac01b835860b2de6cf7ad9fa8
SiHoon61 <dlatlgns000@gmail.com>
2024-10-17 17:11:32 +0900

    fix: title 이건모약 -> 이게모약

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,57 +1,44 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 //link
-import { KAKAO_LINK } from '../../config/config';
+import { KAKAO_LINK } from "../../config/config";
 
 //img
-import kakao from '../../assets/Login/kakao.png'
-import google from '../../assets/Login/google.png'
+import kakao from "../../assets/Login/kakao.png";
+import google from "../../assets/Login/google.png";
 
 import {
-    LogoBox,
-    Logo,
-    LogoName,
-    LoginContainer,
-    JoinContainer,
-    Description,
-    SNSIcon,
-} from './style';
+  LogoBox,
+  Logo,
+  LogoName,
+  LoginContainer,
+  JoinContainer,
+  Description,
+  SNSIcon,
+} from "./style";
 const Login = () => {
-    const navigate = useNavigate();
-    const kakaoLoginHandler = () => {
-        window.location.href = KAKAO_LINK;
-    };
-    return (
-
-        <LoginContainer>
-            <LogoBox>
-                <Logo />
-                <LogoName>
-                    이건모약
-                </LogoName>
-            </LogoBox>
-            <Description>
-                간편 소셜로그인
-            </Description>
-            <JoinContainer>
-                
-                <SNSIcon
-                    src={kakao}
-                    alt='kakao'
-                    onClick={kakaoLoginHandler}
-                />
-                <SNSIcon
-                    src={google}
-                    alt='google'
-                    onClick={() => navigate('/chatroom')}
-                />
-            </JoinContainer>
-
-        </LoginContainer>
-
-
-    );
+  const navigate = useNavigate();
+  const kakaoLoginHandler = () => {
+    window.location.href = KAKAO_LINK;
+  };
+  return (
+    <LoginContainer>
+      <LogoBox>
+        <Logo />
+        <LogoName>이게모약</LogoName>
+      </LogoBox>
+      <Description>간편 소셜로그인</Description>
+      <JoinContainer>
+        <SNSIcon src={kakao} alt="kakao" onClick={kakaoLoginHandler} />
+        <SNSIcon
+          src={google}
+          alt="google"
+          onClick={() => navigate("/chatroom")}
+        />
+      </JoinContainer>
+    </LoginContainer>
+  );
 };
 
 export default Login;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,68 +1,64 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
 import {
-    Header,
-    LogoBox,
-    Logo,
-    LogoName,
-    Body,
-    NewChat,
-    PlusImg,
-    Description,
-} from './style';
+  Header,
+  LogoBox,
+  Logo,
+  LogoName,
+  Body,
+  NewChat,
+  PlusImg,
+  Description,
+} from "./style";
 
-import ChatList from '../../components/ChatList/ChatList';
- 
+import ChatList from "../../components/ChatList/ChatList";
+
 const Main = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const cameraHandler = () => {
-        navigate("/camera");
-    }
-    
-    const [data, setData] = useState([]);
+  const cameraHandler = () => {
+    navigate("/camera");
+  };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/getData`);
-                setData(response.data);
-                console.log(response.data);
-            } catch (err) {
-                console.log(err)
-            }
-        };
+  const [data, setData] = useState([]);
 
-        fetchData();
-    }, []); 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_SERVER_URL}/getData`
+        );
+        setData(response.data);
+        console.log(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
+    fetchData();
+  }, []);
 
-    return (
-        <>
-            <Header>
-                <LogoBox>
-                    <Logo />
-                    <LogoName >
-                        이건모약
-                    </LogoName>
-                </LogoBox>
-            </Header>
-            <Body>
-                <NewChat onClick={cameraHandler}>
-                    <PlusImg />
-                    <Description>
-                        새로운 알약 검색
-                    </Description>
-                </NewChat>
-                {data.map((item, index) => (
-                    <ChatList key={index} item={item} />
-                ))}
-
-            </Body>
-        </>
-    );
+  return (
+    <>
+      <Header>
+        <LogoBox>
+          <Logo />
+          <LogoName>이게모약</LogoName>
+        </LogoBox>
+      </Header>
+      <Body>
+        <NewChat onClick={cameraHandler}>
+          <PlusImg />
+          <Description>새로운 알약 검색</Description>
+        </NewChat>
+        {data.map((item, index) => (
+          <ChatList key={index} item={item} />
+        ))}
+      </Body>
+    </>
+  );
 };
- 
+
 export default Main;
