5426f772457029a74110be700d0c79067650798d
SiHoon61 <dlatlgns000@gmail.com>
2024-11-04 21:39:11 +0900

    fix: chatList ingredient -> pillName

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -1,33 +1,29 @@
-import React from 'react';
+import React from "react";
 import {
-    ChatContainer,
-    ImgBox,
-    DataBox,
-    Title,
-    IngredientBox,
-    IngredientItem,
-    Date,
-} from './style';
-import testImg from '../../assets/testImg.png';
+  ChatContainer,
+  ImgBox,
+  DataBox,
+  Title,
+  IngredientBox,
+  IngredientItem,
+  Date,
+} from "./style";
+import testImg from "../../assets/testImg.png";
 
 const ChatList = ({ item }) => {
-    return (
-        <ChatContainer>
-            <ImgBox src={testImg} alt="pillImg" />
-            <DataBox>
-                <Title>
-                    {item.title}
-                </Title>
-                <IngredientBox>
-                    {item.ingredient.map((ingredientData, index) => (
-                        <IngredientItem key={index}>
-                            {ingredientData}
-                        </IngredientItem>
-                    ))}
-                </IngredientBox>
-            </DataBox>
-        </ChatContainer>
-    );
+  return (
+    <ChatContainer>
+      <ImgBox src={testImg} alt="pillImg" />
+      <DataBox>
+        <Title>{item.title}</Title>
+        <IngredientBox>
+          {item.pillName.map((pillName, index) => (
+            <IngredientItem key={index}>{pillName}</IngredientItem>
+          ))}
+        </IngredientBox>
+      </DataBox>
+    </ChatContainer>
+  );
 };
 
 export default ChatList;
